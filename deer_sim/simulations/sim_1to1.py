8645a12638aac154745f87b919dbb9272addb7b7
janzen <janzenchoi@yahoo.com>
2024-08-24 16:12:06 -0500

    Fixed stress distribution and elastic strain for CPFE

--- deer_sim/simulations/sim_1to1.py
+++ deer_sim/simulations/sim_1to1.py
@@ -400,7 +400,7 @@ SIMULATION_FORMAT = """
 # ==================================================
 
 [Outputs]
-  exodus = true
+  # exodus = true
   print_linear_residuals = false
   [./console]
     type        = Console
