419dd750221b42a2ec2551cb44666a4a6d2031d1
janzen <janzenchoi@yahoo.com>
2024-09-13 14:40:37 -0500

    Changed minimal sim test

--- deer_sim/simulations/1to1_ui.py
+++ deer_sim/simulations/1to1_ui.py
@@ -387,8 +387,10 @@ SIMULATION_FORMAT = """
   # Timestep growth
   [./TimeStepper]
     type                   = IterationAdaptiveDT
-    growth_factor          = 1.5
-    cutback_factor         = 0.67
+    # growth_factor          = 1.5
+    # cutback_factor         = 0.67
+    growth_factor          = 3.0
+    cutback_factor         = 0.33
     linear_iteration_ratio = 100000000000
     optimal_iterations     = 8
     iteration_window       = 1
