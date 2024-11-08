01c0fd13420c0c242003ad49f09000213e843657
Janzen <janzenchoi@yahoo.com>
2024-11-08 13:12:04 +1100

    Added new params after initial optimisation iteration

--- scripts/summarise/sum_ui.py
+++ scripts/summarise/sum_ui.py
@@ -17,7 +17,7 @@ from moose_sim.analyse.plotter import Plotter, save_plot
 RESULTS_DIR = "/mnt/c/Users/janzen/OneDrive - UNSW/PhD/results/moose_sim"
 SAMPLED_PATH = f"{RESULTS_DIR}/2024-11-04 (617_lh2_40um_sm)"
 SIM_PATHS = [f"{RESULTS_DIR}/{sim_dir}" for sim_dir in [
-    # "2024-11-06 (617_lh2_40um_opt)",
+    "2024-11-07 (617_lh2_40um_opt)",
 ]]
 SUMMARY_FILE = "617_s3_40um_lh2_sampled.csv"
 
