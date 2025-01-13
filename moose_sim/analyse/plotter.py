de04978340f6a8c1479c87ab6e07098fa698134f
Janzen <janzenchoi@yahoo.com>
2025-01-13 16:04:26 +1100

    Minor changes

--- moose_sim/analyse/plotter.py
+++ moose_sim/analyse/plotter.py
@@ -54,7 +54,7 @@ class Plotter:
         plt.figure(figsize=(5,5))
         plt.title(title, fontsize=size+3, fontweight="bold", y=1.05)
         plt.gca().set_position([0.17, 0.12, 0.75, 0.75])
-        plt.gca().grid(which="major", axis="both", color="SlateGray", linewidth=1, linestyle=":")
+        plt.gca().grid(which="major", axis="both", color="SlateGray", linewidth=1, linestyle=":", alpha=0.5)
 
         # Set x and y labels
         x_unit_str = f" ({self.x_units})" if self.x_units != "" else ""
